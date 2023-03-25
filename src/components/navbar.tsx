fe24fdd8a2929f81419b1257755dfa6c6d8ecbe3
Miguel Rivas <mrivaslezama@gmail.com>
2023-03-24 22:30:22 -0300

    first commit

---
+++ src/components/navbar.tsx
@@ -0,0 +1,44 @@
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg blur border-radius-sm top-0 z-index-3 shadow position-sticky py-3 start-0 end-0">
+      <div className="container px-1">
+        <a className="navbar-brand font-weight-bolder ms-lg-0 " href="https://www.creative-tim.com/astro">Autana Muebles</a>
+        <button className="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse" data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon mt-2">
+            <span className="navbar-toggler-bar bar1"></span>
+            <span className="navbar-toggler-bar bar2"></span>
+            <span className="navbar-toggler-bar bar3"></span>
+          </span>
+        </button>
+        <div className="collapse navbar-collapse" id="navigation">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <a className="nav-link text-dark font-weight-bold d-flex align-items-center me-2 " aria-current="page" href="/astro-ecommerce/">
+                Inicio
+              </a>
+            </li>
+            <li className="nav-item dropdown">
+              <a className="nav-link text-dark dropdown-toggle font-weight-bold d-flex align-items-center me-2 " aria-current="page" id="pagesExample" data-bs-toggle="dropdown" aria-expanded="false">
+                Paginas
+              </a>
+              <ul className="dropdown-menu" aria-labelledby="pagesExample">
+                <li><a className="dropdown-item" href="/astro-ecommerce/landing/">Nosotros</a></li>
+                <li><a className="dropdown-item" href="/astro-ecommerce/product/">Trabajos</a></li>
+               
+              </ul>
+            </li>
+            
+            <li className="nav-item">
+              <a className="nav-link text-dark font-weight-bold d-flex align-items-center me-2 " aria-current="page" href="https://wa.me/c/56948446060">
+                <i className="fab text-lg fa-whatsapp"></i>
+              </a>
+            </li>
+            
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
